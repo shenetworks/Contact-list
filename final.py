e7871c3adc9d3df8069306914b5a89e0b2db8e18
shenetworks <serena@shenetworks.tech>
2023-05-01 14:59:27 -0500

    final

--- final.py
+++ final.py
@@ -2,7 +2,7 @@ import json
 from os import path
 import input_validation 
 
-phonebook = [{"First": "Serena", "Last": "DiPenti", "Middle": "M", "Phone": "1112223333"}, {"First": "Steve", "Last": "Smith", "Middle": "J", "Phone": "1112224444"}]
+phonebook = [{"First": "Serena", "Last": "DiPenti", "Middle": "M", "Phone": "+1(555) 555-5555"}, {"First": "Steve", "Last": "Smith", "Middle": "J", "Phone": "+1(444) 444-5555"}]
 
 filename = "phonebook.json"
 
@@ -15,45 +15,61 @@ if path.isfile(filename) is False:
 def add_person():
     #user input validation 
     name = input_validation.name_validation()
-    new_entry = name
+    # format {"First": "Serena", "Last": "DiPenti", "Middle": "M"}
+
     phone = input_validation.phone_validation()
-    new_entry['Phone'] = phone
+    name['Phone'] = phone
+    first_name = name["First"]
     
     #adding the new entry to the JSON document
     data = json.load(open('phonebook.json'))
-    data.append(new_entry)
-    with open('phonebook.json', 'w') as phonebook_json:
-        json.dump(data, phonebook_json)
+    # data.append(name)
+    # with open('phonebook.json', 'w') as phonebook_json:
+    for entry in range(len(data)):
+        if data[entry]["Phone"] == str(phone):
+            print("This number already exists in the phonebook")
+            break
+    else: 
+        with open('phonebook.json', 'w') as phonebook_json:
+                data.append(name)
+                json.dump(data, phonebook_json)
+                print(f"{first_name} has been added to the phonebook")
     
-    #confirming contact has been successfully added
-    print("Contact has been added to the phonebook")  #update this to include the firstname formatting
 
 #delete an entry by name
 def del_person_name():
     #user input validation
     name = input_validation.name_validation()
     
-    # format {"First": "Serena", "Last": "DiPenti", "Middle": "M"}
-    # assigning key values to variables
-    first = name["First"]
-    last = name["Last"]
+    # format example {"First": "Serena", "Last": "DiPenti", "Middle": "M"}
+    # assigning user input key values to string variables
+    first = str(name["First"])
+    last = str(name["Last"])
+
     #loading json file
     data = json.load(open('phonebook.json'))
-    # matching user input with existing contact data in json
+    # matching user input with existing entry data in json
     for entry in range(len(data)):
-        if data[entry]["Last"] == str(last) and data[entry]["First"] == str(first):
-            confirm = input(f"Would you like to delete {entry} Y/N: ") # needs to print out whole dictonary to confirm 
+        if str(data[entry]["Last"]).lower() == last.lower() and str(data[entry]["First"]).lower() == first.lower():
+            phone = data[entry]["Phone"]
+            confirm = input(f"Would you like to delete {first} {last} - {phone} Y/N: ") 
             if confirm == "Y":
                 del data[entry]    
                 with open('phonebook.json', 'w') as phonebook_json:
                     json.dump(data, phonebook_json)
-                    print(f"{first} has been deleted to the phonebook")
+                    print(f"{first} has been deleted from the phonebook")
                     break
             else:
                 print("Exiting.")
                 break
     else:
         print(f"{first} {last} is not in the phonebook")
+        search_option = input("Would you like to try searching by phone number? Y/N: ")
+        if search_option == "Y":
+            del_person_phone()
+        else:
+            print("Exiting")
+
 
 #delete an entry by phone number
 def del_person_phone():
@@ -64,15 +80,22 @@ def del_person_phone():
     # number = input("Enter phone number of contact you would like to delete: ")
     for entry in range(len(data)):
         if data[entry]["Phone"] == str(phone):
-            confirm = input(f'Would you like to delete {phone} ? Y/N: ') # add name of who the number belongs to
+            f_name = data[entry]["First"]
+            l_name = data[entry]["Last"]
+            confirm = input(f'Would you like to delete {f_name} {l_name} - {phone} ? Y/N: ') # add name of who the number belongs to
             if confirm == "Y":
                 del data[entry]    
                 with open('phonebook.json', 'w') as phonebook_json:
                     json.dump(data, phonebook_json)
-                    print(f"{phone} has been deleted to the phonebook")
+                    print(f"{phone} has been deleted from the phonebook")
                     break
     else:
         print(f"{phone} is not in the phonebook")
+        search_option_1 = input("Would you like to try searching by phone number? Y/N: ")
+        if search_option_1 == "Y":
+            del_person_name()
+        else:
+            print("Exiting")
 
 #list all current saved entries
 def list_entries():
--- input_validation.py
+++ input_validation.py
@@ -3,17 +3,18 @@ import re
 
 #user input name validation
 def name_validation():
-    user_input_name = input("Enter the name <lastname> <firstname> <middleinitial>: ")
-    pattern = re.compile('([A-Za-z]{1,25}[\'\-]?[A-Za-z]{1,25}?[\,]?) ([A-Za-z]{1,25}[\,\'\.\-]?)? ([A-Za-z]{1}[\,\'\.\-]?)?') # last-name first M
+    user_input_name = str(input("Enter the name <lastname> <firstname> <middleinitial(optional)>: "))
+    pattern = re.compile('^([A-Za-z]{1,25}[\'\-]?[A-Za-z]{1,25}?[\,]?) ([A-Za-z]{1,25}[\,\'\.\-]?)?([A-Za-z]{1}[\,\'\.\-]?)?$') # last-name first M
     
     if re.findall(pattern, user_input_name):
-        named_list = re.search(pattern, user_input_name) ### need to make capitalization standardized 
+        named_list = re.search(pattern, user_input_name)  
         last_name = named_list.group(1)
-        if named_list.group(2):
-            first_name = named_list.group(2)
+        first_name = named_list.group(2)
         if named_list.group(3):
-            middle_inital = named_list.group(3)
-        valid_name = {"First": first_name, "Last": last_name, "Middle": middle_inital}
+            middle_inital = named_list.group(3) 
+            valid_name = {"First": first_name, "Last": last_name, "Middle": middle_inital}
+        else:
+            valid_name = {"First": first_name, "Last": last_name}
         return valid_name
     else: 
         print('Your input is not valid, please try again')
@@ -21,12 +22,12 @@ def name_validation():
 
 # user input phone number validation
 def phone_validation():
-    user_input_phone = input('Enter phone number: ')
-    pattern = re.compile('^\(?([0-9]{3})\)?[-.●]?([0-9]{3})[-.●]?([0-9]{4})$') #add additional format options for phone
+    user_input_phone = input('Enter phone number [+][country code][(area code) subscriber number]: ')
+    pattern = re.compile('^([+][0-9]{1,2})[\(]?[0-9]{2,5}[)\- ]?[0-9\- ]{3,10}$') 
 
     if re.findall(pattern, user_input_phone):
         valid_phone = re.search(pattern, user_input_phone)
-        return str(valid_phone.group()) # add standardized format for US numbers
+        return str(valid_phone.group()) 
     else:
         print('Your input is not valid, please try again.')
         phone_validation()
